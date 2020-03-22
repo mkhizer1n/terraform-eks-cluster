6082da18210f61dd8db96efb2698451849478970
root <root@ip-172-31-13-17.ap-south-1.compute.internal>
2020-03-22 07:05:04 +0000

    terraform-eks-cluster

---
+++ variable.tf
@@ -0,0 +1,55 @@
+variable "profile" {
+  description = "AWS User account Profile"
+  default = "terrafrom"
+}
+
+variable "region" {
+  default = "ap-south-1"
+}
+variable "aws_access_key" {
+ type = "string"
+ description = "The account identification key used by your Terraform client."
+ default = "AKIA3QQQQARMJVJKVFFF"
+}
+
+variable "aws_secret_key" {
+ type = "string"
+ description = "The secret key used by your terraform client to access AWS."
+ default = "DlRs70Y7YLwna8eUT3Nobf6fcQnBXPHeaBOAVzsM"
+}
+
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "key" {
+  description = "Enter Key name"
+}
+
+variable "sub_ids" {
+  default = []
+}
+
+variable "instance-ami" {
+  default = "ami-0b99c7725b9484f9e" # AMI of Mumbai region
+}
+
+variable "instance_type" {
+  default = "t3.medium"
+}
+
+
+variable "cluster-name" {
+  description = "Cluster Name"
+}
+
+variable "server-name" {
+  description = "Ec2 Server Name"i
+  default = "eks-master"
+}
+
+variable "vpc_name" {
+  description = "VPC name"
+  default = "eks-vpc"
+}
+  
